acf597ab64f27694ef1bdfb84498ec8e9de6fff8
ketan <ketan4373@gmail.com>
2018-09-15 18:07:53 +0000

    supported external filtercontroller

--- src/ApiModel.php
+++ src/ApiModel.php
@@ -11,8 +11,10 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class ApiModel extends Model
+
+class ApiModel extends Authenticatable
 {
 
     /**
